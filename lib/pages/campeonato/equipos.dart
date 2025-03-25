3b30522285f6beb581fec41e2f774f2c0bba91f3
adimer <adimer101@gmail.com>
2025-03-25 14:32:24 -0400

    colcoado de ultimos

--- lib/pages/campeonato/equipos.dart
+++ lib/pages/campeonato/equipos.dart
@@ -1,5 +1,8 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
 class Equipos extends StatefulWidget {
   final Map? liga;
   const Equipos({
@@ -12,33 +15,100 @@ class Equipos extends StatefulWidget {
 }
 
 class _EquiposState extends State<Equipos> {
+  final url = dotenv.env['API_URL'];
+  List equipos = [];
+
   @override
   void initState() {
     super.initState();
-    if (widget.liga != null) {
-      print(widget.liga!['name']);
+    getEquipo();
+  }
+  getEquipo() async {
+    final ligaId = widget.liga!['id'];
+    final response = await http.get(Uri.parse('$url/equipos?ligaId=$ligaId'));
+    if (response.statusCode == 200) {
+      print(response.body);
+      final res = json.decode(response.body);
+      equipos = res;
+    } else {
+      print('Error');
     }
+    setState(() {});
   }
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        const Text('Equipos'),
-        const Divider(),
+        // const SizedBox(height: 10),
+        // Text(
+        //   'Tabla de posiciones - ${widget.liga?['name']}',
+        //   style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        // ),
+        // const Divider(),
         Expanded(
-          child: ListView.builder(
-            itemCount: 10,
-            itemBuilder: (context, index) {
-              return ListTile(
-                title: Text('Equipo $index'),
-                subtitle: Text('Estadio $index'),
-                leading: Image.asset(
-                  'assets/images/futbol.jpg',
-                  width: 50,
-                ),
-                trailing: const Icon(Icons.arrow_forward_ios),
-              );
-            },
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal, // por si hay muchas columnas
+            child: DataTable(
+              columnSpacing: 10,         // Espacio horizontal entre columnas
+              dataRowMinHeight: 30,      // Altura mínima de las filas
+              dataRowMaxHeight: 30,      // Altura máxima de las filas
+              headingRowHeight: 36,      // Altura del encabezado
+              columns: const [
+                DataColumn(label: Text('#')),
+                DataColumn(label: Text('Equipo')),
+                DataColumn(label: Text('PJ')),
+                DataColumn(label: Text('G')),
+                DataColumn(label: Text('E')),
+                DataColumn(label: Text('P')),
+                DataColumn(label: Text('GF')),
+                DataColumn(label: Text('GC')),
+                DataColumn(label: Text('DG')),
+                DataColumn(label: Text('Pts')),
+                DataColumn(label: Text('Últimos 5')),
+              ],
+              rows: equipos.map<DataRow>((equipo) {
+                return DataRow(cells: [
+                  DataCell(Text('${equipos.indexOf(equipo) + 1}')),
+                  DataCell(Text(equipo['name'] ?? '')),
+                  DataCell(Text('${equipo['pj'] ?? 0}')),
+                  DataCell(Text('${equipo['g'] ?? 0}')),
+                  DataCell(Text('${equipo['e'] ?? 0}')),
+                  DataCell(Text('${equipo['p'] ?? 0}')),
+                  DataCell(Text('${equipo['gf'] ?? 0}')),
+                  DataCell(Text('${equipo['gc'] ?? 0}')),
+                  DataCell(Text('${equipo['dg'] ?? 0}')),
+                  DataCell(Text('${equipo['pts'] ?? 0}')),
+                  DataCell(Row(
+                    children: (equipo['ultimos5'] as List<dynamic>).map((r) {
+                      IconData icon;
+                      Color color;
+
+                      switch (r) {
+                        case 'Ganó':
+                          icon = Icons.check_circle;
+                          color = Colors.green;
+                          break;
+                        case 'Perdió':
+                          icon = Icons.cancel;
+                          color = Colors.red;
+                          break;
+                        case 'Empate':
+                          icon = Icons.remove_circle;
+                          color = Colors.orange;
+                          break;
+                        default:
+                          icon = Icons.help;
+                          color = Colors.grey;
+                      }
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 2.0),
+                        child: Icon(icon, size: 16, color: color),
+                      );
+                    }).toList(),
+                  )),
+                ]);
+              }).toList(),
+            ),
           ),
         ),
       ],
